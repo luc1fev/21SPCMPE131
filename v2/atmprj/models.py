b12334335866a886d31139151d6acd138ab8945f
leonidas <10078186+luc1fev@users.noreply.github.com>
2021-04-15 20:48:33 -0700

    reg ok

--- v2/atmprj/models.py
+++ v2/atmprj/models.py
@@ -7,3 +7,6 @@ class Accounts(models.Model):
 	amount = models.DecimalField(default = 0,max_digits = 10,decimal_places = 2)
 	pwd = models.CharField(max_length = 64)
 	upload = models.FileField(upload_to = 'uploads/',blank = True,null = True)
+
+	def __str__(self):
+		return self.name
--- v2/atmprj/views.py
+++ v2/atmprj/views.py
@@ -2,51 +2,66 @@ from django.shortcuts import render
 from . import models
 # Create your views here.
 
+indexpg = 'indexpage/index.html'
+indexDemo = 'index_demo.html'
+
+loginpg = indexpg
+login_demo = 'login_demo.html'
+
+# user =models.Accounts.objects
+
 def index(request):
 	pass
-	return render(request,'index_demo.html')
+	return render(request,indexDemo)
 
 def login(request):
 	if request.method=="POST":
-		user_name= request.POST.get('the_id')
+		user_id= request.POST.get('the_id')
 		pass_word =request.POST.get('the_pwd')
 		try:
-			user = models.Accounts.objects.get(name = user_name)
-			if user.name == user_name and user.pwd==pass_word:
+			user = models.Accounts.objects.get(identi = user_id)
+			if str(user.identi) == user_id and user.pwd==pass_word:
 				return render(request,'login_demo.html',{'msg':'ok'})
 			# todo redirect to account page????
-
 			else:
 				return render(request,'login_demo.html',{'msg':'wrong'})
 		except:
 			return render(request,'login_demo.html',{'msg':'catch part,no user'})
-
-	return render(request,'login_demo.html')
+	return render(request,login_demo)
 
 def register(request):
 	msg =""
 	if(request.method=="POST"):
 		new_username= request.POST.get('the_id')
-		new_password =request.POST.get('the_pwd')
+		new_password = request.POST.get('the_pwd')
+		new_password2 = request.POST.get('the_pwd2')
 		try:
-			# user = models.Accounts.objects.get(name = new_username)
-			# if user:
-			# 	# todo!! need huge modify from BOA tell us about yourself page.
-			# 	return render(request,'register_demo.html',{'msg':'name taken'})
-			# 	# todo needs change login name with digit id??
-			# else:
-			new_user = models.Accounts()
-			new_user.name=new_username
-			new_user.pwd=new_password
-			new_user.save()
-			return render(request,'register_demo.html',{'msg':'GOOD! more!'})
-
+			if new_password2 and new_password and new_username:
+				if new_password2 == new_password:
+					new_user = models.Accounts()
+					new_user.name=new_username
+					new_user.pwd=new_password
+					new_user.save()
+					return render(request,'register_demo.html',{'msg':'GOOD! more!'})
+				else:
+					return render(request,'register_demo.html',{'msg':'passcode not match'})
+			else:
+				return render(request,'register_demo.html',{'msg':'fill all boxes!'})
 		except:
 			return render(request,'register_demo.html',{'msg':'catch part'})
 
 	return render(request,'register_demo.html',{'msg':msg})
 
 def transfer(request):
+
+	# user_from = models.Accounts.objects.get(identi = user.)
+	# user_to = ''
+	# try:
+	# 	userfrom = models.Accounts.objects.get(identi = user_from)
+	# 	userto = models.Accounts.objects.get(identi = user_to)
+	# 	if userfrom and userto
+	# except:
+
 	# todo transfer page from Chase bank just pure js logic with arthorization
 	# meaningless for download page as frame
 	# todo take the css and just build one
